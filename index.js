8f3c7e334b7262026c5e226fa96bf789f63742d0
HomeToken <stephan.thierry@live.com>
2021-06-04 13:28:05 +0200

    await page.setUserAgent

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function getURL(url) {
 async function getURLWithJavascript(url, waitForSelector) {
     let browser = await puppeteer.launch({ headless: true });
     let page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
     await page.setDefaultNavigationTimeout(global_timeout);
     await page.goto(url);
 
