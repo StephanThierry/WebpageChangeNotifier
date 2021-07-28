5569c96dae90677c9277271a09ee4959cf7f103b
HomeToken <stephan.thierry@live.com>
2021-07-28 18:20:19 +0200

    Fixed cuncurrency issues

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ async function getURL(url) {
     return ((await axios.get(url)).data);
 }
 
-async function getURLWithJavascript(url, waitForSelector) {
-    let browser = await puppeteer.launch({ headless: true });
+async function getURLWithJavascript(browser, url, waitForSelector) {
+    
     let page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
     await page.setDefaultNavigationTimeout(global_timeout);
@@ -33,18 +33,21 @@ async function getURLWithJavascript(url, waitForSelector) {
     }
 
     let response = await page.evaluate(() => document.documentElement.outerHTML);
-    await browser.close();
+    let pages = await browser.pages();
+    for(let i=0; i<pages.length;i++){
+        await pages[i].close();
+    }
     return (response);
 }
 
-async function detectPageChange(url, validate, change, positiveChange, decodeJS, waitForSelector) {
+async function detectPageChange(browser, url, validate, change, positiveChange, decodeJS, waitForSelector) {
     let resString;
     let found = false;
     try {
         let response = "";
 
         if (decodeJS) {
-            response = await getURLWithJavascript(url, waitForSelector);
+            response = await getURLWithJavascript(browser, url, waitForSelector);
         } else {
             response = await getURL(url);
         }
@@ -82,17 +85,20 @@ async function detectPageChange(url, validate, change, positiveChange, decodeJS,
 
 const checkSites = [
     { URL: "https://www.proshop.dk/Spillekonsol/Sony-PlayStation-5-Nordic/2831713", validate: "Pick-up Points lagerstatus", change: "ukendt leveringsdato", decodeJS: true, waitForSelector: '.site-currency-attention' },
-    { URL: "https://www.bilka.dk/elektronik/computere-og-gaming/konsoller-og-spil/playstation-5/playstation-5-konsoller/pl/ps5-konsol/", validate: "PlayStation 5 konsoller", change: "no-results", decodeJS: true, waitForSelector: '.products-container'  },
+    { URL: "https://www.bilka.dk/elektronik/computere-og-gaming/konsoller-og-spil/playstation-5/playstation-5-konsoller/pl/ps5-konsol/", validate: "PlayStation 5 konsoller", change: "no-results", decodeJS: true, waitForSelector: '.products-container' },
     { URL: "https://www.elgiganten.dk/product/gaming/konsoller/playstation-konsoller/220276/playstation-5-ps5", validate: "PlayStation 5", change: "Ukendt leveringsdato" },
     { URL: "https://www.power.dk/gaming-og-underholdning/playstation/playstation-konsoller/playstation-5/p-1077687/", validate: "Collect", change: "stock-unavailable", decodeJS: true, waitForSelector: '.buy-area__webshop' },
     { URL: "https://cdon.dk/spil/playstation/playstation-5-konsol/", validate: "Playstation 5 - Konsol", change: "produktet er faret vild", decodeJS: true, waitForSelector: '.product-page-header__title' },
 ]
 function performCheck() {
-    checkSites.forEach(function (item) {
-        (async function (item) {
-            await detectPageChange(item.URL, item.validate, item.change, item.positiveChange, item.decodeJS, item.waitForSelector);
-        })(item)
-    })
+    (async function () {
+        let browser = await puppeteer.launch({ headless: true });
+        for (let i = 0; i < checkSites.length; i++) {
+            item = checkSites[i];
+            await detectPageChange(browser, item.URL, item.validate, item.change, item.positiveChange, item.decodeJS, item.waitForSelector);
+        }
+        await browser.close();
+    })()
 
     let minutes = 20;
     console.log(new Date().toString() + " - Que next run in " + minutes + " minues.");
